4d780ad59824968b2c616364459149e2fbf352fb
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-03-17 06:23:40 -0700

    pushcommit

---
+++ LoopingSwitchCase_213.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main (){
+    int counter;
+    int i;
+    int perulanganwhile = 0;
+    int perulanganDo = 0;
+//loop with for
+    //loop fowr ascending
+    for (counter = 0; counter < 5; counter++){
+        cout << "Nilai Counter = " << counter << "TI UMY" << endl;
+    }
+    cout << "Nilai counter terakhir = " << counter << endl;
+    cout <<endl;
+    //loop with for
+    //loop for descending
+    for (counter = 5; counter > 0; counter--){
+        cout << "Nilai Counter = " << counter << "TI UMY" << endl;
+    }
+    cout << "Nilai counter terakhir = " << counter << endl;
+    cout <<endl;
+
+    //loop with while
+    srand (time(0));
+    i = rand() % 10;
+    
